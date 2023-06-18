219033a6279801359d63d2df0653259a9dbdbd97
Effisal <136177774+Effisal@users.noreply.github.com>
2023-06-18 23:31:14 +0300

    add_st

--- public/js/controls.js
+++ public/js/controls.js
@@ -110,3 +110,20 @@ $(document).on('click', '.add_book', function(){
 });
 
 });
+
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scrollBtn").style.display = "block";
+    } else {
+      document.getElementById("scrollBtn").style.display = "none";
+    }
+  }
+  
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
